eb9a5fcb76ba4526c898c40747219e60cc6577f1
Amir Pasha Zarghami <apashazarghami@gmail.com>
2022-10-09 18:41:44 -0700

    make days of week

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,7 @@ const container = document.querySelector(".container");
 const setCity = () => {
     const searchInput = document.querySelector(".search-input");
     city = searchInput.value;
+    searchInput.value = ""
 }
 
 const createMainWeather = (image, temperature, city, country) => {
@@ -47,19 +48,58 @@ const createMainWeather = (image, temperature, city, country) => {
     container.appendChild(mainDiv);
 }
 
+const setDay = () => {
+    const divDays = document.createElement("div");
+    const sunSpan = document.createElement("span");
+    const monSpan = document.createElement("span");
+    const tueSpan = document.createElement("span");
+    const wedSpan = document.createElement("span");
+    const thuSpan = document.createElement("span");
+    const friSpan = document.createElement("span");
+    const satSpan = document.createElement("span");
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    divDays.classList.add("days");
+    sunSpan.innerText = "Sun";
+    monSpan.innerText = "Mon";
+    tueSpan.innerText = "Tue";
+    wedSpan.innerText = "Wed";
+    thuSpan.innerText = "Thu";
+    friSpan.innerText = "Fri";
+    satSpan.innerText = "Sat";
+    divDays.appendChild(sunSpan);
+    divDays.appendChild(monSpan);
+    divDays.appendChild(tueSpan);
+    divDays.appendChild(wedSpan);
+    divDays.appendChild(thuSpan);
+    divDays.appendChild(friSpan);
+    divDays.appendChild(satSpan);
+    container.appendChild(divDays);
+    const today = days[new Date().getDay()];
+    const daysSpan = document.querySelector(".container").children[2].children;
+    for(let item of daysSpan) {
+        if (item.innerText === today) {
+            item.classList.add("day")
+        }
+    }
+}
+
 const serachHandler = () => {
     setCity();
-    fetch(`http://api.weatherstack.com/forecast?access_key=6d05179956ec17bb7f0488a8ccd48eed&query=${city}`)
-        .then(response => response.json())
-        .then(json => {
-            const imageSrc = json.current.weather_icons[0];
-            const temperature = json.current.temperature;
-            const city = json.location.name;
-            const country = json.location.country;
-            createMainWeather(imageSrc, temperature, city, country);
-            // saveToLocalStorage(json);
-        });
+    if(container.children.length === 1) {
+        fetch(`http://api.weatherstack.com/forecast?access_key=6d05179956ec17bb7f0488a8ccd48eed&query=${city}`)
+            .then(response => response.json())
+            .then(json => {
+                const imageSrc = json.current.weather_icons[0];
+                const temperature = json.current.temperature;
+                const city = json.location.name;
+                const country = json.location.country;
+                createMainWeather(imageSrc, temperature, city, country);
+                // saveToLocalStorage(json);
+                setDay();
+            });
+    }
 }
+
 
 // const saveToLocalStorage = (weatherInformation) => {
 //     let weatherInformations;
